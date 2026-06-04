e1dd0074c9f3f2e35b625f692ced8e3037813ad9
messrseylull <elifyildirim.35.25@gmail.com>
2026-06-04 18:21:16 +0300

    Add files via upload

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ function connectToDatabase() {
   db = mysql.createConnection({
     host: process.env.DB_HOST || 'localhost',
     user: process.env.DB_USER || 'root',
-    password: process.env.DB_PASSWORD || '',
+    password: process.env.DB_PASSWORD || 'deneme',
   });
 
   db.connect((err) => {
